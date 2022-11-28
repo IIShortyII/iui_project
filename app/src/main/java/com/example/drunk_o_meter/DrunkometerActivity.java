a4bab3df4a82c1e3c43413d13fbbc93e6e107922
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2022-11-28 16:35:12 +0100

    Add basic activity navigation

---
+++ app/src/main/java/com/example/drunk_o_meter/DrunkometerActivity.java
@@ -0,0 +1,14 @@
+package com.example.drunk_o_meter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrunkometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drunkometer);
+    }
+}
--- app/src/main/java/com/example/drunk_o_meter/MainActivity.java
+++ app/src/main/java/com/example/drunk_o_meter/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.drunk_o_meter;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,14 +31,20 @@ public class MainActivity extends AppCompatActivity {
         // determine mode
         if (UserData.USERNAME.length() == 0){
             // go to onboarding to provide username
-
+            Intent intent = new Intent(MainActivity.this, OnboardingActivity.class);
+            intent.putExtra("stage", "username");
+            MainActivity.this.startActivity(intent);
         } else if (UserData.BASELINE_TYPING.size() != BASELINE_TEXT_COUNT){
             // go to onboarding to provide baseline typing samples
+            Intent intent = new Intent(MainActivity.this, OnboardingActivity.class);
+            intent.putExtra("stage", "baseline");
+            MainActivity.this.startActivity(intent);
 
         } else {
             // go to actual app
+            Intent intent = new Intent(MainActivity.this, DrunkometerActivity.class);
+            MainActivity.this.startActivity(intent);
 
         }
-        //setContentView(R.layout.activity_main);
     }
 }
