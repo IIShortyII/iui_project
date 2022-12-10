555ef231403e4a12d604d1a8ebea3021f16b716e
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2022-12-10 21:44:26 +0100

    Add calculation of mean and completiontime for baseline typing samples

--- app/src/main/java/com/example/drunk_o_meter/userdata/UserData.java
+++ app/src/main/java/com/example/drunk_o_meter/userdata/UserData.java
@@ -14,6 +14,10 @@ public class UserData {
      */
     public static ArrayList<TypingSample> BASELINE_TYPING_CHALLENGE = new ArrayList<TypingSample>();
 
+    public static double MEAN_ERROR_BASELINE;
+
+    public static double MEAN_COMPLETIONTIME_BASELINE;
+
     /**
      * Username
      */
@@ -23,6 +27,24 @@ public class UserData {
      * List of all current typing challenge samples
      */
     public static Set<TypingSample> TYPING_CHALLENGE = new HashSet<TypingSample>();
+
+    /**
+     * Calculate the mean value of the provided variable
+     * @param type the variable used to calculate the mean
+     * @return the mean of the provided variable
+     */
+    public static double calculateMean(String type){
+        double mean = 0.0;
+        for(int i =0; i< BASELINE_TYPING_CHALLENGE.size(); i++){
+            if (type.equals("error")){
+                mean = mean + BASELINE_TYPING_CHALLENGE.get(i).getError();
+            } else if (type.equals("completiontime")){
+                mean = mean + BASELINE_TYPING_CHALLENGE.get(i).getTime();
+            }
+        }
+        return mean / BASELINE_TYPING_CHALLENGE.size();
+    }
+
 
 
 }
