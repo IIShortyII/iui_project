eb6830cbe4bb8caea9f4e2821285a866cce6768e
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2022-12-23 11:41:44 +0100

    Adjust jar files with Git Large Storage Files

--- app/src/main/java/com/example/drunk_o_meter/userdata/DrunkometerAnalysis.java
+++ app/src/main/java/com/example/drunk_o_meter/userdata/DrunkometerAnalysis.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ba519f27398e8bae4d56d261d4fbba0ddc1d8808683845c0057cf584bcb716d
-size 80
+oid sha256:5eda19280d9b7fcd7de7ae7887716b6d74f2e69b31513836bd591eb09d17858d
+size 241
