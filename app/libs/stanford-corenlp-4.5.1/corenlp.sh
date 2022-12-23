29d5aa35225634e22875bbe601fcc977a390bab9
Kathrin Schnizer <kathrin.schnizer@gmail.com>
2022-12-23 14:39:37 +0100

    Fix lfs bug

--- app/libs/stanford-corenlp-4.5.1/corenlp.sh
+++ app/libs/stanford-corenlp-4.5.1/corenlp.sh
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4f23a17def5cbcc1658145d97455abc589f10bf2c282ca76b109c6d4eac96e71
-size 871
+#!/usr/bin/env bash
+#
+# Runs Stanford CoreNLP.
+# Simple uses for xml and plain text output to files are:
+#    ./corenlp.sh -file filename
+#    ./corenlp.sh -file filename -outputFormat text 
+# Split into sentences, run POS tagger and NER, write CoNLL-style TSV file:
+#    ./corenlp.sh -annotators tokenize,ssplit,pos,lemma,ner -outputFormat conll -file input.txt
+# You can also start a simple shell where you can enter sentences to be processed:
+#    ./corenlp.sh
+
+OS=`uname`
+# Some machines (older OS X, BSD, Windows environments) don't support readlink -e
+if hash readlink 2>/dev/null; then
+  scriptdir=`dirname $0`
+else
+  scriptpath=$(readlink -e "$0") || scriptpath=$0
+  scriptdir=$(dirname "$scriptpath")
+fi
+
+echo java -mx5g -cp \"$scriptdir/*\" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
+java -mx5g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
