d07c174701991e92f488eff535a8391e300674cd
Katharina Danner <dannerk@in.tum.de>
2023-01-09 18:53:40 +0100

    Adapted detail view of chats

--- app/src/main/java/com/example/drunk_o_meter/chat_list/ChatDetailViewFragment.java
+++ app/src/main/java/com/example/drunk_o_meter/chat_list/ChatDetailViewFragment.java
@@ -1,14 +1,19 @@
 package com.example.drunk_o_meter.chat_list;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.drunk_o_meter.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +33,8 @@ public class ChatDetailViewFragment extends Fragment {
     private String time;
     private String content;
     private boolean safeToText;
+
+    private View layout;
 
     public ChatDetailViewFragment() {
         // Required empty public constructor
@@ -69,9 +76,32 @@ public class ChatDetailViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat_detail_view, container, false);
+        layout = inflater.inflate(R.layout.fragment_chat_detail_view, container, false);
+
+        Log.d("Chat Detail", "content: "+ this.content);
+        ImageView selfieView = layout.findViewById(R.id.chat_detail_selfie);
+        TextView contentView = layout.findViewById(R.id.chat_detail_content);
+        TextView dateView = layout.findViewById(R.id.chat_detail_date);
+        TextView timeView = layout.findViewById(R.id.chat_detail_time);
+        TextView safeToTextView = layout.findViewById(R.id.chat_detail_safeToText);
+
+        //TODO: change selfie
+        contentView.setText(content);
+        dateView.setText(date);
+        timeView.setText(time);
+
+        if (safeToText) {
+            safeToTextView.setText("Safe to text");
+            safeToTextView.setTextColor(Color.GREEN);
+        } else {
+            safeToTextView.setText("Not safe to text");
+            safeToTextView.setTextColor(Color.RED);
+
+        }
+
+        Log.d("Chat Detail", "content view: " + contentView.getText());
+        return layout;
     }
 
-    //TODO: insert back button to layout
+
 }
